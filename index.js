da68cda1e54637e4aae241038cf55bb0eabb3927
MeissamOaziz <meissam@oaziz.ca>
2025-02-24 10:30:54 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'famecommerce.html'));
+});
+
+const port = 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
+});
